dcda31e13a64242408091e51df4f6a58a6e935fe
Marlen Buri <ch-murmeli@gmx.ch>
2017-11-25 16:50:17 +0100

    basic library structure stands

---
+++ db/migrate/20171125133723_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.integer :author_id
+      t.string :title
+      t.string :color
+      t.string :isbn
+      t.integer :genre_id
+
+      t.timestamps null: false
+    end
+  end
+end
